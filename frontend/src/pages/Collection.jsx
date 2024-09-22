12e49905227f9ea69ea23d8dbb5a12a44a22eecc
Maja Petkova <majapetkova1@yahoo.com>
2024-09-22 13:10:29 +0200

    Update Collection page

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,8 +1,24 @@
+import { useContext, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
 
 const Collection = () => {
-  return (
-    <div>Collection</div>
-  )
-}
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
 
-export default Collection
+  return (
+    <div className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t ">
+      {/* Filter options */}
+
+      <div className="min-w-60">
+        <p className="my-2 text-xl flex items-center cursor-pointer gap-2">
+          FILTERS
+        </p>
+        {/* Category filter */}
+
+        <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? "" :"hidden" } `}></div>
+      </div>
+    </div>
+  );
+};
+
+export default Collection;
