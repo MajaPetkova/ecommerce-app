a3f5abe1845465790d3b607e410a8e9a46442481
Maja Petkova <majapetkova1@yahoo.com>
2024-10-18 22:51:50 +0200

    Add related products to the component

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -1,5 +1,7 @@
 import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+import ProductItem from "./ProductItem";
 
 const RelatedProducts = ({ category, subCategory }) => {
   const { products } = useContext(ShopContext);
@@ -11,12 +13,23 @@ const RelatedProducts = ({ category, subCategory }) => {
 
       productsCopy = productsCopy.filter((x) => category === x.category);
       productsCopy = productsCopy.filter((x) => subCategory === x.subCategory);
- 
-      console.log(productsCopy.slice(0, 5));
+
+      setRelated(productsCopy.slice(0, 5));
     }
   }, [products]);
 
-  return <div>RelatedProducts</div>;
+  return (
+    <div className="my-24">
+      <div className="text-center text-3xl py-2">
+        <Title text1={"RELATED"} text2={"PRODUCTS"}></Title>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {related.map((x, i) => 
+            <ProductItem key={i} id={x._id} name={x.name} price={x.price} image={x.image} />      
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default RelatedProducts;
