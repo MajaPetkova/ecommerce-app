f3778720bb456d09382ceb71d10a7417d79d9375
Maja Petkova <majapetkova1@yahoo.com>
2024-11-02 10:43:00 +0100

    Add button and submitHandler for login (login page)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,8 +3,13 @@ import { useState } from "react";
 const Login = () => {
   const [currentState, setCurrentState] = useState("Sign Up");
 
+  const onSubmitHandler= async(e) =>{
+    e.preventDefault();
+
+  }
+
   return (
-    <form className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-black">
+    <form  onSubmit={onSubmitHandler} className="flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-black">
       <div className="inline-flex items-center gap-2 mb-2 mt-10">
         <p className="prata-regular text-3xl">{currentState}</p>
         <hr className="border-none h-[1.5px] w-8 bg-gray-800"/>
@@ -16,6 +21,7 @@ const Login = () => {
         <p className="cursor-pointer">Forgot your password</p>
         {currentState ==="Login" ? <p className="cursor-pointer" onClick={()=>setCurrentState("Sign Up")}>Create Account</p>: <p className="cursor-pointer" onClick={()=>setCurrentState("Login")}>Login Here</p>  }
       </div>
+      <button className="text-white bg-black px-8 py-3 mt-4 font-light">{currentState==="Login"? "Sign In":"Sign Up"}</button>
     </form>
   );
 };
