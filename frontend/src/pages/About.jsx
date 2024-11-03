4ee7d1631ee4d10dd5d167892cda3e88ed9c1df2
Maja Petkova <majapetkova1@yahoo.com>
2024-11-03 19:29:05 +0100

    Update template for About page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,4 +1,5 @@
 import { assets } from "../assets/assets";
+import NewsletterBox from "../components/NewsletterBox";
 import Title from "../components/Title";
 
 const About = () => {
@@ -41,7 +42,23 @@ const About = () => {
       <div className="text-xl py-4">
         <Title text1={"WHY"} text2={"CHOOSE US"}/>
       </div>
-      <div className="flex flex-col md:flex-row text-sm mb-20"></div>
+      <div className="flex flex-col md:flex-row text-sm mb-20">
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Quality Assurance:</b>
+          <p className="text-gray-600">We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Convenience:</b>
+          <p className="text-gray-600">With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+        </div>
+        <div className="border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5">
+          <b>Exceptional Customers Service:</b>
+          <p className="text-gray-600">Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.  </p>
+        </div>
+      </div>
+      <NewsletterBox/>
+
+
     </div>
   );
 };
