3206e4fd9691e6e072781fbd4edef66331406292
Maja Petkova <majapetkova1@yahoo.com>
2024-11-11 19:16:47 +0100

    Create  user route and controller
    Add register functionality

---
+++ backend/controllers/userController.js
@@ -0,0 +1,53 @@
+import User from "../models/userModel.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
+
+const createToken= (id) =>{
+    return jwt.sign({id}, process.env.JWT_SECRET)
+}
+
+// route for user login
+const loginUser = async (req, res) => {};
+
+// route for user registration
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const exist = await User.findOne({ email });
+
+    // checking if user exist
+    if (exist) {
+      return res.json({ success: false, message: "User already exist" });
+    }
+    // validation email format and strong password
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: "Email is not valid" });
+    }
+    if (password.length < 5) {
+      return res.json({
+        success: false,
+        message: "Please enter a strong password",
+      });
+    }
+    // hashing user password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({ name, email, password:hashedPassword });
+    const user = await newUser.save();
+
+    const token = createToken(user._id);
+    res.json({success:true, token})
+
+  } catch (err) {
+    console.log(err);
+    res.json({success:false, message:err.message })
+  }
+};
+
+//route for admin login
+const loginAdmin = async (req, res) => {};
+
+export { loginUser, registerUser, loginAdmin };
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config"
 import connectDB from "./config/mongodb.js";
 import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 
 //App config
 const app = express();
@@ -12,7 +13,10 @@ connectCloudinary()
 
 //middlewares 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
+
+// api endpoints 
+app.use("/api/user/", userRouter)
 
 
 //api endpoints
