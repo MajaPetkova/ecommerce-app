8e361de9a688724e70035801011a885d1bf74046
Maja Petkova <majapetkova1@yahoo.com>
2025-06-09 11:42:37 +0200

    Add admin middlewear

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,7 +70,7 @@ const registerUser = async (req, res) => {
 const loginAdmin = async (req, res) => {
   try {
     const { email, password } = req.body;
-    console.log(process.env.ADMIN_EMAIL)
+    
     if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
       const token = jwt.sign(email + password, process.env.JWT_SECRET)
       res.json({success:true, token})
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,17 +6,18 @@ import {
   addProduct,
 } from "../controllers/productController.js";
 import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
 productRouter.get("/list", getProducts);
-productRouter.post("/add",upload.fields([
+productRouter.post("/add",adminAuth,upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
     { name: "image3", maxCount: 1 },
     { name: "image4", maxCount: 1 },
   ]), addProduct);
 productRouter.post("/single", singleProduct);
-productRouter.post("/remove", removeProduct);
+productRouter.post("/remove",adminAuth, removeProduct);
 
 export default productRouter;
