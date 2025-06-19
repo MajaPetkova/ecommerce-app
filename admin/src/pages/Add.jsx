a25f7ce16ada9681139640ecbfdac4b0e082d3cf
Maja Petkova <majapetkova1@yahoo.com>
2025-06-19 19:40:05 +0200

    Add submitHandler functionality

--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import { useState } from 'react'
+import axios from 'axios'
+import { backendUrl } from '../App'
 
-const Add = () => {
+const Add = ({token}) => {
 
     const [image1, setImage1] = useState(false);
     const [image2, setImage2] = useState(false);
@@ -10,16 +12,40 @@ const Add = () => {
     const [image4, setImage4] = useState(false);
 
     const [name, setName] = useState("");
-    const [desc, setDesc] = useState("");
+    const [description, setDescription] = useState("");
     const [price, setPrice]= useState("");
     const [category, setCategory] = useState("Men");
-    const [subCategory, setSubCategory] = useState("Upperwear");
+    const [subcategory, setSubcategory] = useState("Upperwear");
     const [bestseller, setBestseller] = useState(false);
     const [sizes, setSizes] = useState([])
 
+   const onSubmitHandler = async(e) =>{
+     e.preventDefault()
+     try{
+       const formData = new FormData();
+     
+       formData.append("name", name)
+       formData.append("description", description)
+       formData.append("price", price)
+       formData.append("category", category)
+       formData.append("subcategory", subcategory)
+       formData.append("bestseller", bestseller)
+       formData.append("sizes", JSON.stringify(sizes))
+
+       image1 && formData.append("image1", image1)
+       image2 && formData.append("image2", image2)
+       image3 && formData.append("image3", image3)
+       image4 && formData.append("image4", image4)
+
+       const res = await axios.post(backendUrl + "/api/product/add", formData, {headers : {token}})
+       console.log(res.data)
+     }catch(err){ 
+        console.log(err)
+     }
+   }
 
   return (
-    <form className='flex flex-col w-full items-start gap-3'>
+    <form  onSubmit={onSubmitHandler} className='flex flex-col w-full items-start gap-3'>
         <div>
             <p className='mb-2'>Upload Image</p>
             <div className='flex gap-2'>
@@ -47,7 +73,7 @@ const Add = () => {
         </div>
         <div className='w-full'>
             <p className='mb-2'>Product description</p>
-        <textarea onChange={(e)=>setDesc(e.target.value)} value={desc} className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here' required/>
+        <textarea onChange={(e)=>setDescription(e.target.value)} value={description} className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here' required/>
         </div>
         <div className='flex flex-col sm:flex-row gap-2 w-full sm:gap-8'>
             <div>
@@ -60,7 +86,7 @@ const Add = () => {
             </div>
             <div>
                 <p className='mb-2'>Sub category</p>
-                <select onChange={(e)=>setSubCategory(e.target.value)} className='w-full px-3 py-2'>
+                <select onChange={(e)=>setSubcategory(e.target.value)} className='w-full px-3 py-2'>
                     <option value="Topwear">Topwear</option>
                     <option value="Bottomwear">Bottomwear</option>
                     <option value="Winterwear">Winterwear</option>
