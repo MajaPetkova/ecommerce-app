d871a5db67dafa4d3f037dab25d75740ddf6cfd6
Maja Petkova <majapetkova1@yahoo.com>
2025-08-10 17:41:04 +0200

    Create middleware to authenticate user

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addToCard, updateCart, getUserCard } from "../controllers/cartController.js";
+import authUser from "../middleware/auth.js";
 
 const cartRouter= express.Router();
 
-cartRouter.post('/get', getUserCard);
-cartRouter.post('/update', updateCart);
-cartRouter.post('/add', addToCard);
+cartRouter.post('/get',authUser, getUserCard);
+cartRouter.post('/update',authUser, updateCart);
+cartRouter.post('/add',authUser, addToCard);
 
 export default cartRouter;
